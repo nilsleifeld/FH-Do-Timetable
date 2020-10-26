33a853ffe61b39c62941cd4ccce90a6dc0ef971b
Nils Leifeld <nils.leifeld@adesso.de>
2020-10-26 09:56:08 +0100

    build timetable

--- src/app/table-generator/table-generator.component.ts
+++ src/app/table-generator/table-generator.component.ts
@@ -1,6 +1,8 @@
+import { jsPDF } from 'jspdf';
 import {Component} from '@angular/core';
 import {FhModelService} from './_services/fh-model/fh-model.service';
 import {FhModul} from './_interfaces/fh-modul';
+import * as html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-table-generator',
@@ -15,6 +17,8 @@ export class TableGeneratorComponent {
   public days: string[] = [ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
 
   public selectDay: string = this.days[0];
+
+  public buildTable: FhModul[] = null;
 
   constructor(
     private readonly fhModelService: FhModelService
@@ -41,12 +45,28 @@ export class TableGeneratorComponent {
   }
 
   public onBuild(): void {
-    const buildTable: FhModul[] = [];
+    this.buildTable = [];
 
     this.tables.forEach(table => {
       const selectModuls = table.filter(m => m.select);
-      buildTable.push(...selectModuls);
+      this.buildTable.push(...selectModuls);
     });
+  }
+
+  /**
+   * FIXME the downloded document is emty?
+   */
+  public downloadTimetable(): void {
+    const content: Element = window.document.getElementById('timetable-build');
+
+    const options = {
+      name: 'output.pdf',
+      image: { type: 'jpeg' },
+      html2canvas: {},
+      jsPDF: { orientation: 'landscape' }
+    };
+
+    html2pdf().from(content).set(options).save();
   }
 
   public setSelectDay(day: string): void {
@@ -58,6 +78,17 @@ export class TableGeneratorComponent {
 
     this.tables.forEach(modules => {
       const toPushModuls = modules.filter(m => m.weekday === this.selectDay);
+      result.push(toPushModuls);
+    });
+
+    return result;
+  }
+
+  public getTableWeekFromBuild(): Array<FhModul[]> {
+    const result: Array<FhModul[]> = [];
+
+    this.days.forEach(day => {
+      const toPushModuls = this.buildTable.filter(m => m.weekday === day);
       result.push(toPushModuls);
     });
 
